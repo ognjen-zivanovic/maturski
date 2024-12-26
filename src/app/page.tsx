7ab15a2829ecdee4a31faa9cc546b0d406e33699
ognjenzivanovic <ognjen.zivanovic06@gmail.com>
2024-12-26 15:09:17 +0100

    very basic reservation page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,67 @@
+"use client";
+
 export default function Home() {
-	return <div>Hello world</div>;
+	function validate(e: React.ChangeEvent<HTMLInputElement>) {
+		const value = e.target.value;
+		if (value !== "") {
+			if (parseInt(value) < 0) {
+				e.target.value = "0";
+			} else if (parseInt(value) > 23) {
+				e.target.value = "23";
+			}
+		}
+	}
+
+	return (
+		<div>
+			<div className="p-4">
+				<div>Name</div>
+				<input className="rounded-lg border-2 border-blue-500 pl-2 pr-2" type="text" />
+			</div>
+			<div className="p-4">
+				<div>Surname</div>
+				<input className="rounded-lg border-2 border-blue-500 pl-2 pr-2" type="text" />
+			</div>
+			<div className="p-4">
+				<div>Phone number</div>
+				<input className="rounded-lg border-2 border-blue-500 pl-2 pr-2" type="text" />
+			</div>
+			<div className="flex flex-row flex-wrap gap-4">
+				<div className="m-4">
+					<div>Check in date</div>
+					<input className="h-8 rounded-lg border-2 border-blue-500" type="date" />
+				</div>
+				<div className="m-4">
+					<div>Time (hours) </div>
+					<input
+						id="number"
+						type="number"
+						defaultValue="12"
+						min="0"
+						max="23"
+						className="h-8 w-12 rounded-lg border-2 border-blue-500 pl-2"
+						onChange={(e) => validate(e)}
+					/>
+				</div>
+			</div>
+			<div className="flex flex-row flex-wrap gap-4">
+				<div className="m-4">
+					<div>Check out date</div>
+					<input className="h-8 rounded-lg border-2 border-blue-500" type="date" />
+				</div>
+				<div className="m-4">
+					<div>Time (hours) </div>
+					<input
+						id="number"
+						type="number"
+						defaultValue="12"
+						min="0"
+						max="23"
+						className="h-8 w-12 rounded-lg border-2 border-blue-500 pl-2"
+						onChange={(e) => validate(e)}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
